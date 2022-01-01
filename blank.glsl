ab17026a0dd9c8a5ebc1dbad0975d0a4b5ee8314
WebEye <web-eye@gmx.net>
2022-01-01 18:56:07 +0100

    initial comit

---
+++ blank.glsl
@@ -0,0 +1,12 @@
+/*
+2D LED Spectrum - Visualiser - https://www.shadertoy.com/view/Mlj3WV#
+Based on Led Spectrum Analiser by: simesgreen - 27th February, 2013 - https://www.shadertoy.com/view/Msl3zr
+2D LED Spectrum by: uNiversal - 27th May, 2015
+Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+*/
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+   
+    // fragColor = vec4(ledColor, 1.0);
+}
